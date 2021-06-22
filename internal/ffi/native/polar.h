e924fbd9a19b4f581b5afbb1f412107cac4678ca
osohq <dev@steve.computer>
2021-06-22 20:08:49 +0000

    v0.13.0

--- internal/ffi/native/polar.h
+++ internal/ffi/native/polar.h
@@ -79,3 +79,7 @@ int32_t polar_free(polar_Polar *polar);
  * it can be properly freed
  */
 int32_t query_free(polar_Query *query);
+
+int32_t polar_enable_roles(polar_Polar *polar_ptr);
+
+int32_t polar_validate_roles_config(polar_Polar *polar_ptr, const char *validation_query_results);
